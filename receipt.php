99afe130b74ff0b120258ceb9921f6c8e78c7bdb
Parth Mudgal <parth.mudgal@halodoc.com>
2018-07-04 01:00:21 +0530

    remove assets

--- receipt.php
+++ receipt.php
@@ -58,7 +58,6 @@ if ($result == "FAIL") {
       This should never be used in your integration -->
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
 <html>
-<link rel="stylesheet" type="text/css" href="assets/paymentstyle.css"/>
 <head>
     <title>Receipt</title>
     <meta http-equiv="Content-Type" content="text/html, charset=iso-8859-1">
